082015b64f8a4baeb590c24cc730855cc35cd00d
Vitaly Nikolaev <vit3011@rambler.ru>
2015-08-26 16:28:11 +0300

    Current

--- control_server.py
+++ control_server.py
@@ -5,15 +5,8 @@ import os
 import time
 import subprocess as sub
 import threading
-from datetime import datetime, timedelta
 
-INTERVAL = 5 # Интервал проверки наличия новых сообщений (обновлений) на сервере в секундах
-ADMIN_ID = 54052993 # ID пользователя. Комманды от других пользователей выполняться не будут
-TEMP_ID = 0
-flag=0
-URL = 'https://api.telegram.org/bot' # Адрес HTTP Bot API
-TOKEN = '94799839:AAEOHMOexXt-X-3tnMCPT2VzC1b63fTpo9c' # Ключ авторизации для Вашего бота
-offset = 0
+
 current_time = 0
 now_plus_10 = 0
 pid = 0
@@ -24,21 +17,6 @@ def make_url_query_string(params):
 
 
 def check_updates(limit=5):
-
-    global offset, TEMP_ID, pid
-    params = make_url_query_string({'offset': offset+1, 'limit': limit, 'timeout': 0})
-    request = requests.get(URL + TOKEN + '/getUpdates' + params) # Отправка запроса обновлений
-
-    if not request.status_code == 200: return False # Проверка ответа сервера
-    if not request.json()['ok']: return False # Проверка успешности обращения к API
-    if not request.json()['result']: return False # Проверка наличия обновлений в возвращенном списке
-
-    for update in request.json()['result']: # Проверка каждого элемента списка
-        offset = update['update_id'] # Извлечение ID сообщения
-        from_id = update['message']['from']['id'] # Извлечение ID отправителя
-        name = update['message']['from']['first_name']
-        surname = update['message']['from']['last_name']
-        message = update['message']['text']
 
         if(ADMIN_ID == from_id) or (from_id == TEMP_ID and zero_access()):
             if (message == "break" and pid != 0):
@@ -91,31 +69,9 @@ def send_respond(text, chat_id):
     return True
 
 
-def login(id):
-    global TEMP_ID, curent_time, now_plus_10
-    TEMP_ID = id
-    curent_time = datetime.now()
-    now_plus_10 = curent_time + timedelta(minutes=10)
 
 
-def create_files():
-    if not (os.path.isfile('auth_msg.log') and os.path.isfile('not_auth_msg.log')):
-        f1 = open('not_auth_msg.log', 'w')
-        f2 = open('auth_msg.log', 'w')
-        f1.close()
-        f2.close()
 
-
-def zero_access():
-    global current_time, now_plus_10, TEMP_ID
-    if (datetime.now() < now_plus_10):
-        return True
-    else:
-        TEMP_ID = 0
-        return False
-
-
-create_files()
 
 while True:
     try:
