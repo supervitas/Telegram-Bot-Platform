e93b2903bfc606f148b3cc9a2f60178cb959cd3b
Vitaly Nikolaev <vit3011@rambler.ru>
2015-08-26 17:36:29 +0300

    Current

--- telegram_handler.py
+++ telegram_handler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nikolaev'
 
+# import control_server
 import requests
 import os
 from datetime import datetime, timedelta
@@ -8,8 +9,7 @@ from datetime import datetime, timedelta
 
 INTERVAL = 5  # Интервал проверки наличия новых сообщений (обновлений) на сервере в секундах
 ADMIN_ID = 54052993  # ID пользователя. Комманды от других пользователей выполняться не будут
-TEMP_ID = 0  # Временный ID админа. Нужен в случае,если потребуется выполнять комманды не со своего аккаунта в телеграме
-             # Присваивается отправлением пароля
+TEMP_ID = 0  # Временный ID админа.Присваивается отправлением пароля
 flag = 0
 URL = 'https://api.telegram.org/bot'  # Адрес HTTP Bot API
 TOKEN = '94799839:AAEOHMOexXt-X-3tnMCPT2VzC1b63fTpo9c'  # Ключ авторизации для Вашего бота
@@ -40,22 +40,29 @@ def check_updates(limit=5):
         surname = update['message']['from']['last_name']
         message = update['message']['text']
 
+        if (ADMIN_ID == from_id) or (from_id == TEMP_ID and zero_access()):
+            Logger.log_auth_user(message, from_id, name, surname)
+        if (ADMIN_ID != from_id) or (from_id != TEMP_ID):
+            Logger.log_notauth_user(message, from_id, name, surname)
 
 
-def login(id): # Функция авторизации длится 10 минут
-    global TEMP_ID, curent_time, now_plus_10
-    TEMP_ID = id
-    curent_time = datetime.now()
-    now_plus_10 = curent_time + timedelta(minutes=10)
+class Auth:
+    @staticmethod
+    def login(id): # Функция авторизации длится 10 минут
+        global TEMP_ID, current_time, now_plus_10
+        TEMP_ID = id
+        current_time = datetime.now()
+        now_plus_10 = current_time + timedelta(minutes=10)
 
+    @staticmethod
+    def zero_access():  # Проверка истечения админки
+        global current_time, now_plus_10, TEMP_ID
+        if (datetime.now() < now_plus_10):
+            return True
+        else:
+            TEMP_ID = 0
+            return False
 
-def zero_access(): # Проверка истечения админки
-    global current_time, now_plus_10, TEMP_ID
-    if (datetime.now() < now_plus_10):
-        return True
-    else:
-        TEMP_ID = 0
-        return False
 
 
 class Logger:  # Класс отвечает за создание лог файлов, и логирование
@@ -63,24 +70,24 @@ class Logger:  # Класс отвечает за создание лог фай
         pass
 
     @staticmethod
-    def log_notauth_user(self,message,from_id,name,surname):
+    def log_notauth_user(message, from_id, name, surname):
         f = open('not_auth_msg.log', 'a')
         f.write("Message - %s    id - %s Name - %s %s " %(message, from_id, name, surname) + "  %s" % datetime.now() + '\n')
 
     @staticmethod
-    def log_auth_user(self,message,from_id,name,surname):
+    def log_auth_user(message, from_id, name, surname):
         f = open('auth_msg.log', 'a')
         f.write("Message - %s    id - %s Name - %s %s " %(message, from_id, name, surname) + "  %s" % datetime.now() + '\n')
 
     @staticmethod
-    def create_files(self): # Создаем файлы логов
+    def create_files(): # Создаем файлы логов
         if not (os.path.isfile('auth_msg.log') and os.path.isfile('not_auth_msg.log')):
             f1 = open('not_auth_msg.log', 'w')
             f2 = open('auth_msg.log', 'w')
             f1.close()
             f2.close()
 
-
+Logger.create_files()
 
 while True:
 
