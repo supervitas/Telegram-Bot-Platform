13b17848b950ee680d7a68bf068caf9084be8d33
Vitaly Nikolaev <vit3011@rambler.ru>
2015-08-28 16:23:21 +0300

    Add example

--- modules/example.py
+++ modules/example.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nikolaev'
-import main_handler
+from main_handler import Respond
 
 
 def handler(message, user_id):  # функция которая принимает сообщение от главного модуля(Должна быть реализована обязательно)
 
-    main_handler.Respond.send_text_respond(message, user_id)  # отправляем обратно
+    Respond.send_text_respond(message, user_id)  # отправляем обратно
